4474e08066932ca72a362f3eea9e9aa819c2a1c2
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-09-12 23:38:01 -0700

    changed

--- src/app.rs
+++ src/app.rs
@@ -1307,9 +1307,11 @@ impl App {
 
                 // Update light borders in main thread; if changed, emit a dedicated event
                 if let Some(lb) = light_borders {
-                    let _changed = self.gs.lighting.update_borders(cx, cz, lb);
-                    // Always notify neighbors so they can pick up new micro seam planes
-                    self.queue.emit_now(Event::LightBordersUpdated { cx, cz });
+                    let changed = self.gs.lighting.update_borders(cx, cz, lb);
+                    // Only notify neighbors when borders actually change to avoid cascades
+                    if changed {
+                        self.queue.emit_now(Event::LightBordersUpdated { cx, cz });
+                    }
                 }
             }
             Event::ChunkRebuildRequested { cx, cz, cause } => {
